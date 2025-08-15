071db5d31eba6173b1a2f919250761d86b301f19
John Mark Sarino <161050884+JMjeyem@users.noreply.github.com>
2025-08-15 18:12:17 +0800

    table-update

--- resources/views/product/product-list.blade.php
+++ resources/views/product/product-list.blade.php
@@ -6,6 +6,26 @@
         <div class="card-header">
             <h2>Product List</h2>
         </div>
+        <div class="card-body">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td>John</td>
+                        <td>Doe</td>
+                        <td>@social</td>
+                        </tr>
+                    </tbody>
+                </table>
+        </div>
     </div>
 </div>
 
