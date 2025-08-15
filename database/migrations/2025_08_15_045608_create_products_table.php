bc33740233833cfc105879fef18938ac7e65e89c
John Mark Sarino <161050884+JMjeyem@users.noreply.github.com>
2025-08-15 15:42:31 +0800

    Initial commit

---
+++ database/migrations/2025_08_15_045608_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string(column: 'name');
+            $table->string(column:'description')->nullable();
+            $table->integer(column:'quantity');
+            $table->float(column: 'price');
+            $table->string(column: 'image')->nullable();
+            $table->enum(column: 'status', allowed: ['active','in-active'])->default(value: 'active');
+            $table->foreignId('category_id')->constrained(table: 'categories')->onDelete(action: 'cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ resources/views/product/product-list.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.layout')    
+@section('content')
+
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            Product List
+        </div>
+    </div>
+</div>
+
+@endsection
